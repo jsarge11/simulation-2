dc402f8feaefe90b7e251a167c6490589895f06d
Jay Sargent <sargentjay11@gmail.com>
2018-04-13 09:56:45 -0600

    react

---
+++ houser/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Dashboard from './components/Dashboard/Dashboard'
+import Header from './components/Header/Header'
+
+import Wizard from './components/Wizard/Wizard'
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Dashboard/>
+        <Header/>
+        <Wizard/>
+      </div>
+    );
+  }
+}
+
+export default App;
